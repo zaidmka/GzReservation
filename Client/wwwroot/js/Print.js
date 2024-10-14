2a155120abfcf75994ed4a7abed50b1723afe0be
zaidmka <83184605+zaidmka@users.noreply.github.com>
2024-10-14 16:00:30 +0300

    working in reservation recipit!

---
+++ Client/wwwroot/js/Print.js
@@ -0,0 +1,12 @@
+﻿function printDiv(divId) {
+    var divContents = document.getElementById(divId).innerHTML;
+    var printWindow = window.open('', '', 'height=600,width=800');
+    printWindow.document.write('<html><head><title>Print DIV</title>');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(divContents);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+}
